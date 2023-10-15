b6232983db1aa898ba94e62b3cd0b29b2119446e
Samson Kinyanjui <sammainah98@gmail.com>
2023-10-15 16:00:25 +0300

    Map and Filters

---
+++ Chapter1/Maps.js
@@ -0,0 +1,42 @@
+Languages=["Javascript",
+     "Pythons",  
+       "Java",   
+     "Javascript",
+       "Pythons",
+         "Java", 
+    "Javascript",
+      "Pythons", 
+        " Java", 
+     "Javascript",
+       "Pythons",
+         "Java ",
+    "Javascript",
+    "Pythons",
+   " Java",
+    "Javascript",
+    "Pythons",
+    "Java",
+    "Javascript",
+    "Pythons",
+    "Java",
+    "Javascript",
+    "Pythons",
+    "Java",
+    "Javascript",
+    "Pythons",
+   " Java",
+    "Javascript",
+   " Pythons",
+    "Java",
+   " Pythons",
+    "Java"
+    ]
+
+    // Languages.map(function(value){
+    //     console.log(value)
+    // })
+    // filter()
+    const members=Languages.filter((members)=>{
+        return  console.log(members.length >5)
+
+    })
---
+++ Random.py
@@ -0,0 +1,11 @@
+name='Javascript'
+name1='Pythons'
+name2='Java'
+
+count=0
+
+while(count<=10):
+    print(name)
+    print(name1)
+    print(name2)
+    count += 1
