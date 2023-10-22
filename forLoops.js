1797b938b7b4fc25452b8949549472298d02c2da
Samson Kinyanjui <sammainah98@gmail.com>
2023-10-22 14:58:39 +0300

    For loops

---
+++ forLoops.js
@@ -0,0 +1,15 @@
+
+
+
+//Define a function that is called numgenerator
+const numgenerator =() =>{
+    for(let i=0;i<=1000;i++)
+{
+    console.log("Counting Still continues:",i)
+}
+
+
+}
+
+// Call the function above
+numgenerator();
