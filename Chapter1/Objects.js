a3f62be39c3002426f73e41935504238f383a860
Samson Kinyanjui <sammainah98@gmail.com>
2023-10-22 11:55:44 +0300

    Third Commit

---
+++ Chapter1/Objects.js
@@ -0,0 +1,16 @@
+let person={
+    name:'Samson Kinyanjui',
+    age:25,
+    sports:["Football","Chess"],
+    address:{
+     street:"Kingo'ngo'",
+     town:"Nyeri"
+
+    },
+
+    //function
+    allsports:function(){
+        console.log(this.sports);
+    }
+    
+};
