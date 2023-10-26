2b9a3f89d97fef7cc9d0792a0acaefa8f14fc906
Samson Kinyanjui <sammainah98@gmail.com>
2023-10-26 21:15:44 +0300

    Functions forEach(),filter() and map()

---
+++ Arrays3.js
@@ -0,0 +1,22 @@
+let Age=[15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]
+
+Age.forEach((array)=>{
+    console.log(array)
+})
+
+//filter()
+let names=["samson","Kinyanjui","Wanderi","Kiragu","Felix","Lopuran"]
+
+names.filter((number)=>{
+    if (number.length<5){
+        console.log(number)
+    }
+    else{
+        console.log(number)
+    }
+
+})
+Age.map((number)=>{
+    return number*2
+})
+
