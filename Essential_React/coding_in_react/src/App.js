6584aadbc868c81aa113095c8396ce12a42b26dd
Samson Kinyanjui <sammainah98@gmail.com>
2023-10-27 23:28:07 +0300

    React Essential concepts

---
+++ Essential_React/coding_in_react/src/App.js
@@ -0,0 +1,19 @@
+
+import './styleDemo.css';
+const Person=()=>{
+
+  let nam="Samson Kinyanjui"
+  let comp="Rebuilding Himself"
+  return (
+    <>
+    <div className="small"> {nam} {comp}
+</div>
+<div className="big"> {nam} {comp}
+</div>
+    </>
+   
+)}
+
+
+
+export default Person
